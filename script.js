c67867e502b9526e9f2ed730c7ac3e75da9c9b7d
Jake Meyers <jakejake92@yahoo.com>
2020-12-25 19:17:19 -0700

    html done, a little css and javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const currentPrincipal = document.getElementById("currentPrincipal");
+const annualAddition = document.getElementById("annualAddition");
+const yearsToGrow = document.getElementById("yearsToGrow");
+const interestRate = document.getElementById("interestRate");
+const futureValue = document.getElementById("futureValue");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
